503d1127eeebd97ff4becbdaa0247642a595baa4
Sitraka ANDRIATIANA <sitraka.joel.andriatiana@esti.mg>
2022-05-25 09:40:40 +0300

    feat: api inscription

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ const {USER_FIND} = require("../services/const");
 const {only} = require("../services/utility");
 const UserModel = mongoose.model("User", userSchema);
 
-router.post("/login", function (req, res) {
+router.post("/login", async function (req, res) {
     if (req.body.email && req.body.password) {
-        const user = UserModel.findOne({
+        const user = await UserModel.findOne({
             email: req.body.email,
             hashed_password: req.body.password
         });
@@ -28,7 +28,31 @@ router.post("/login", function (req, res) {
             data: {user: only(user, USER_FIND)},
         });
     }
-})
+});
+
+router.post("/inscriptiion", async function (req, res) {
+    if (req.body.email && req.body.password && req.body.nom && req.body.age) {
+        const body = only(req.body, "email password nom prenom age");
+        await UserModel.init();
+        try {
+            const user = new UserModel(body);
+            await user.save();
+            return res.status(200).json({
+                code: 200,
+                message: "User created",
+                data: {user: only(user, USER_FIND)},
+            });
+
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({
+                code: 500,
+                message: "Internal server error",
+                data: e,
+            });
+        }
+    }
+});
 router.use("/api", api);
 
 module.exports = router;
